c9803930e0e7c854e978de7fd837ce6c648614f9
Algys Ievlev <algys.ievlev@gmail.com>
2019-03-11 12:20:43 +0000

    Confirmation validation, private key load from cfg, send/bcast methods

--- plugins/grandpa_plugin/include/eosio/grandpa_plugin/network_messages.hpp
+++ plugins/grandpa_plugin/include/eosio/grandpa_plugin/network_messages.hpp
@@ -10,6 +10,13 @@ using namespace chain;
 
 
 struct chain_conf_msg : public chain_type {
+    chain_conf_msg() = default;
+    chain_conf_msg(const chain_type& chain, const signature_type& sign) {
+        base_block = chain.base_block;
+        blocks = chain.blocks;
+        signature = sign;
+    }
+
     signature_type signature;
 };
 
@@ -21,6 +28,21 @@ struct handshake_msg {
     block_id_type lib;
     signature_type signature;
 };
+
+
+using chain_conf_msg_ptr = shared_ptr<chain_conf_msg>;
+
+chain_conf_msg_ptr make_confirmation(const chain_type& chain, const private_key_type& priv_key) {
+    auto hash = chain::digest_type::hash(chain);
+    auto conf_ptr = std::make_shared<chain_conf_msg>(chain, priv_key.sign(hash));
+    return conf_ptr;
+}
+
+bool validate_confirmation(const chain_conf_msg& chain_conf, const public_key_type& pub_key) {
+    auto chain = chain_type {chain_conf.base_block, chain_conf.blocks};
+    auto hash = chain::digest_type::hash(chain);
+    return public_key_type(chain_conf.signature, hash) == pub_key;
+}
 
 }
 
