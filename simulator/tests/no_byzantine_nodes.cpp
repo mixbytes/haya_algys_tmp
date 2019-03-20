741768cde16503c2f5807c917de2b575a66135d2
Emil Guseynov <theluckyemil@gmail.com>
2019-03-20 20:31:26 +0300

    block-154 Simulator interface

--- simulator/tests/no_byzantine_nodes.cpp
+++ simulator/tests/no_byzantine_nodes.cpp
@@ -1,12 +1,23 @@
 #include <gtest/gtest.h>
 
 #include <string>
+#include <iostream>
+
+#include <simulator.hpp>
+
+using namespace std;
 
 using std::string;
 
 const char *actualValTrue  = "hello gtest";
 const char *expectVal      = "hello gtest";
 
-TEST(StrCompare, CStrEqual) {
-    EXPECT_STREQ(expectVal, actualValTrue);
+TEST(A, B) {
+    TestRunner<> t(3);
+    vector<pair<int, int> > v0{{1, 2}, {2, 10}};
+    graph_type g;
+    g.push_back(v0);
+    t.load_graph(g);
+    assert(delay_matrix[0][1] == 2);
+    t.run();
 }
