f34dc566c97146a5b765a2b7de9309da84a08b00
Algys Ievlev <algys.ievlev@gmail.com>
2019-03-21 16:00:25 +0000

    [bloc-157] store Node pointers in Runner instead of templates

--- simulator/include/simulator.hpp
+++ simulator/include/simulator.hpp
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
+#include <thread>
 
 using namespace std;
 
@@ -29,13 +30,17 @@ private:
     uint32_t now_;
 };
 
+class TestRunner;
 class Node;
+
+using NodePtr = std::shared_ptr<Node>;
+
 
 struct Task {
     uint32_t from;
     uint32_t to;
     uint32_t at;
-    function<void(Node&)> cb;
+    function<void(NodePtr)> cb;
 
     bool operator<(const Task& m) const {
         return at < m.at;
@@ -46,41 +51,52 @@ using matrix_type = vector<vector<int> >;
 using graph_type = vector<vector<pair<int, int>>>;
 
 static Clock tester_clock(0);
-static vector<vector<int> > delay_matrix;
-static priority_queue<Task> timeline;
+
+
+class Network {
+public:
+    Network() = delete;
+    explicit Network(uint32_t node_id, TestRunner* runner):
+        node_id(node_id),
+        runner(runner)
+    { }
+
+    template <typename T>
+    void send(uint32_t to, const T&);
+
+    template <typename T>
+    void bcast(const T&);
+
+private:
+    uint32_t node_id;
+    TestRunner* runner;
+};
 
 
 class Node {
 public:
     Node() = default;
-    explicit Node(int id): id(id) {}
+    explicit Node(int id, Network && net): id(id), net(net) {}
 
-    template<class T>
+    template <typename T>
     void send(uint32_t to, const T& msg) {
-        assert(delay_matrix[id][to] != -1);
-        uint32_t delay = static_cast<uint32_t>(delay_matrix[id][to]);
-        Task action{id, to};
-        action.cb = [&](Node& n) {
-            n.on_receive(msg);
-        };
-        action.at = tester_clock.now() + delay;
-        timeline.push(action);
+        net.send(to, msg);
     }
 
-    template<class T>
-    void on_receive(const T& msg) {
-        std::cout << "Received by " << id << " " << msg << std::endl;
-    };
+    virtual void on_receive(void *) {
+        std::cout << "Received by " << id << std::endl;
+    }
 
-    void on_new_peer_event(uint32_t from) {
+    virtual void on_new_peer_event(uint32_t from)  {
         std::cout << "On new peer event handled by " << id << " at " << tester_clock.now() << endl;
     }
 
     uint32_t id;
     bool is_producer = true;
+    Network net;
 };
 
-template<class TNode = Node>
+
 class TestRunner {
 public:
     TestRunner() = default;
@@ -140,7 +156,9 @@ public:
         delay_matrix = matrix;
     }
 
+    template <typename TNode = Node>
     void run() {
+        init_nodes<TNode>(get_instances());
         init_connections();
         while (!timeline.empty()) {
             auto task = timeline.top();
@@ -150,33 +168,71 @@ public:
         }
     }
 
-    static int get_instances() {
+    uint32_t get_instances() {
         return delay_matrix.size();
     }
 
+    const matrix_type& get_delay_matrix() const {
+        return delay_matrix;
+    }
+
+    void add_task(Task && task) {
+        timeline.push(task);
+    }
+
 private:
+    template <typename TNode>
+    void init_nodes(uint32_t count) {
+        nodes.clear();
+        for (auto i = 0; i < count; ++i) {
+            auto node = std::make_shared<TNode>(i, Network(i, this));
+            nodes.push_back(std::static_pointer_cast<Node>(node));
+        }
+    }
+
     void init_connections() {
         for (uint32_t from = 0; from < get_instances(); from++) {
             for (uint32_t to = 0; to < get_instances(); to++) {
                 int delay = delay_matrix[from][to];
                 if (from != to && delay != -1) {
-                    timeline.push(Task{from, to, static_cast<uint32_t>(delay),
-                                       [from](Node& n){ n.on_new_peer_event(from); }});
+                    add_task(Task{from, to, static_cast<uint32_t>(delay),
+                                       [from](NodePtr n){ n->on_new_peer_event(from); }});
                 }
             }
         }
     }
 
     void init_runner_data(int instances) {
-        nodes.resize(instances);
         delay_matrix.resize(instances);
 
         for (int i = 0; i < instances; i++) {
             delay_matrix[i] = vector<int>(instances, -1);
             delay_matrix[i][i] = 0;
-            nodes[i].id = i;
         }
     }
-    vector<TNode> nodes;
+
+    vector<NodePtr> nodes;
+    vector<vector<int> > delay_matrix;
+    priority_queue<Task> timeline;
 };
 
+
+template <typename T>
+void Network::send(uint32_t to, const T& msg) {
+    auto matrix = runner->get_delay_matrix();
+    assert(matrix[node_id][to] != -1);
+
+    runner->add_task(Task {
+        node_id,
+        to,
+        tester_clock.now() + matrix[node_id][to],
+        [msg](NodePtr n) {
+            n->on_receive(msg);
+        }
+    });
+}
+
+template <typename T>
+void Network::bcast(const T& msg) {
+    //TODO bcast to all nodes with calculate routes
+}
