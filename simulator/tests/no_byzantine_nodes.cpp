b3121254ac1e87bedd6440ca15bc3a9923828a32
Emil Guseynov <theluckyemil@gmail.com>
2019-03-25 13:47:12 +0300

    [block-150] consensus no sync (#22)
    * [bloc-150] consensus no sync
    
    * Вынес slot_ms и blocks_per_slot в параметры конструктора
    
    * Изменил логику шедулинга продюсеров

--- simulator/tests/no_byzantine_nodes.cpp
+++ simulator/tests/no_byzantine_nodes.cpp
@@ -4,20 +4,20 @@
 #include <iostream>
 
 #include <simulator.hpp>
+#include <cstdlib>
+#include <ctime>
+#include <random>
 
 using namespace std;
 
 using std::string;
 
-const char *actualValTrue  = "hello gtest";
-const char *expectVal      = "hello gtest";
-
 TEST(A, B) {
+    srand(66);
     TestRunner t(3);
-    vector<pair<int, int> > v0{{1, 2}, {2, 10}};
+    vector<pair<int, int> > v0{{1, 2}, {2, 5}};
     graph_type g;
     g.push_back(v0);
     t.load_graph(g);
-    assert(t.get_delay_matrix()[0][1] == 2);
     t.run<>();
 }
