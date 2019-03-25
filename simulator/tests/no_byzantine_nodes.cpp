4bf2824d5a0964820d62cf07fabe35a3d23fdaf3
Emil Guseynov <theluckyemil@gmail.com>
2019-03-25 20:51:17 +0300

    Unit tests

--- simulator/tests/no_byzantine_nodes.cpp
+++ simulator/tests/no_byzantine_nodes.cpp
@@ -13,12 +13,31 @@ using namespace std;
 
 using std::string;
 
-TEST(A, B) {
+//TEST(honest_nodes, eos_finality) {
+//    auto t = TestRunner(3);
+//    vector<pair<int, int> > v0{{1, 2}, {2, 10}};
+//    graph_type g;
+//    g.push_back(v0);
+//    t.load_graph(g);
+//    t.add_stop_task(2102);
+//    t.run<Node>();
+//    for (int i = 0; i < 3; i++) {
+//        auto& db = t.get_db(i);
+//        EXPECT_EQ(get_block_height(db.last_irreversible_block_id()), 2);
+//    }
+//}
+
+TEST(honest_nodes, grandpa_finality) {
     auto t = TestRunner(3);
     vector<pair<int, int> > v0{{1, 2}, {2, 10}};
     graph_type g;
     g.push_back(v0);
     t.load_graph(g);
-    assert(t.get_delay_matrix()[0][1] == 2);
+    t.add_stop_task(1503);
     t.run<GrandpaNode>();
+    for (int i = 0; i < 3; i++) {
+        auto& db = t.get_db(i);
+        cout << "Checking node " << i << endl;
+        EXPECT_EQ(get_block_height(db.last_irreversible_block_id()), 2);
+    }
 }
