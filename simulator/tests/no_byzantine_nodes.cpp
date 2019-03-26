7b663895d46a3766d994a8172b407ced832015b7
Emil Guseynov <theluckyemil@gmail.com>
2019-03-26 17:42:24 +0300

    [bloc-151] basic test set

--- simulator/tests/no_byzantine_nodes.cpp
+++ simulator/tests/no_byzantine_nodes.cpp
@@ -36,3 +36,16 @@ TEST(honest_nodes, eos_master_chain_height) {
     EXPECT_EQ(get_block_height(runner.get_db(1).get_master_block_id()), 1);
     EXPECT_EQ(get_block_height(runner.get_db(2).get_master_block_id()), 0);
 }
+
+TEST(honest_nodes, grandpa_finality) {
+    auto runner = TestRunner(3);
+    vector<pair<int, int> > v0{{1, 2}, {2, 10}};
+    graph_type g;
+    g.push_back(v0);
+    runner.load_graph(g);
+    runner.add_stop_task(3 * TestRunner::SLOT_MS);
+    runner.run<GrandpaNode>();
+    EXPECT_EQ(get_block_height(runner.get_db(0).last_irreversible_block_id()), 3);
+    EXPECT_EQ(get_block_height(runner.get_db(1).last_irreversible_block_id()), 3);
+    EXPECT_EQ(get_block_height(runner.get_db(2).last_irreversible_block_id()), 3);
+}
