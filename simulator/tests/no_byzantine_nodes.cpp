1924bd94ccdccae82a316af9578d0e9bbf9864c0
Emil Guseynov <theluckyemil@gmail.com>
2019-03-27 17:13:37 +0300

    Fix block creation (#28)
    * Working tests
    
    * eos finality works
    
    * log GrandpaNode
    
    * fix broken add_stop_task logic

--- simulator/tests/no_byzantine_nodes.cpp
+++ simulator/tests/no_byzantine_nodes.cpp
@@ -18,7 +18,7 @@ TEST(honest_nodes, eos_finality) {
     vector<pair<int, int> > v0{{1, 2}, {2, 10}};
     graph_type g{v0};
     runner.load_graph(g);
-    runner.add_stop_task(4 * TestRunner::SLOT_MS);
+    runner.add_stop_task(7 * runner.get_slot_ms());
     runner.run<Node>();
     EXPECT_EQ(get_block_height(runner.get_db(0).last_irreversible_block_id()), 1);
     EXPECT_EQ(get_block_height(runner.get_db(1).last_irreversible_block_id()), 1);
@@ -27,14 +27,31 @@ TEST(honest_nodes, eos_finality) {
 
 TEST(honest_nodes, eos_master_chain_height) {
     auto runner = TestRunner(3);
-    vector<pair<int, int> > v0{{1, 2}, {2, TestRunner::SLOT_MS}};
+    vector<pair<int, int> > v0{{1, 2}, {2, runner.get_slot_ms()}};
     graph_type g{v0};
     runner.load_graph(g);
-    runner.add_stop_task(TestRunner::SLOT_MS);
+    runner.add_stop_task(runner.get_slot_ms());
     runner.run<Node>();
     EXPECT_EQ(get_block_height(runner.get_db(0).get_master_block_id()), 1);
     EXPECT_EQ(get_block_height(runner.get_db(1).get_master_block_id()), 1);
     EXPECT_EQ(get_block_height(runner.get_db(2).get_master_block_id()), 0);
+}
+
+TEST(honest_nodes, eos_finality_multiple_blocks) {
+    auto runner = TestRunner(3, 2);
+    vector<pair<int, int> > v0{{1, 2}, {2, 10}};
+    graph_type g{v0};
+    runner.load_graph(g);
+    runner.add_stop_task(7 * runner.get_slot_ms());
+    runner.run<Node>();
+    EXPECT_EQ(get_block_height(runner.get_db(0).last_irreversible_block_id()), 2);
+    EXPECT_EQ(get_block_height(runner.get_db(1).last_irreversible_block_id()), 2);
+    EXPECT_EQ(get_block_height(runner.get_db(2).last_irreversible_block_id()), 2);
+
+    auto master = runner.get_db(0).get_master_head();
+    EXPECT_EQ(get_block_height(master->block_id), 14);
+    runner.add_stop_task(13 * runner.get_slot_ms());
+    runner.run_loop();
 }
 
 TEST(honest_nodes, grandpa_finality) {
@@ -43,9 +60,52 @@ TEST(honest_nodes, grandpa_finality) {
     graph_type g;
     g.push_back(v0);
     runner.load_graph(g);
-    runner.add_stop_task(3 * TestRunner::SLOT_MS);
+    runner.add_stop_task(1 * runner.get_slot_ms());
     runner.run<GrandpaNode>();
-    EXPECT_EQ(get_block_height(runner.get_db(0).last_irreversible_block_id()), 3);
-    EXPECT_EQ(get_block_height(runner.get_db(1).last_irreversible_block_id()), 3);
-    EXPECT_EQ(get_block_height(runner.get_db(2).last_irreversible_block_id()), 3);
+    EXPECT_EQ(get_block_height(runner.get_db(0).last_irreversible_block_id()), 1);
+    EXPECT_EQ(get_block_height(runner.get_db(1).last_irreversible_block_id()), 1);
+    EXPECT_EQ(get_block_height(runner.get_db(2).last_irreversible_block_id()), 1);
 }
+
+TEST(honest_nodes, eos_finality_many_nodes) {
+    size_t nodes_amount = 21;
+    auto runner = TestRunner(nodes_amount);
+    vector<pair<int, int> > v0{{1, 20}, {2, 10}, {3, 50}, {4, 30}, {5, 100}};
+    vector<pair<int, int> > v5{{6, 10}, {7, 30}, {8, 20}, {9, 10}, {10, 100}};
+    vector<pair<int, int> > v10{{11, 10}, {12, 10}, {13, 10}, {14, 10}, {15, 100}};
+    vector<pair<int, int> > v15{{16, 10}, {17, 10}, {18, 10}, {19, 10}, {20, 10}};
+    graph_type g(nodes_amount);
+    g[0] = v0;
+    g[5] = v5;
+    g[10] = v10;
+    g[15] = v15;
+    runner.load_graph(g);
+    runner.add_stop_task(31 * runner.get_slot_ms());
+    runner.run<Node>();
+    EXPECT_EQ(get_block_height(runner.get_db(0).last_irreversible_block_id()), 1);
+    EXPECT_EQ(get_block_height(runner.get_db(5).last_irreversible_block_id()), 1);
+    EXPECT_EQ(get_block_height(runner.get_db(10).last_irreversible_block_id()), 1);
+    EXPECT_EQ(get_block_height(runner.get_db(15).last_irreversible_block_id()), 1);
+}
+//
+//TEST(honest_nodes, grandpa_finality_many_nodes) {
+//    size_t nodes_amount = 21;
+//    auto runner = TestRunner(nodes_amount);
+//    vector<pair<int, int> > v0{{1, 20}, {2, 10}, {3, 50}, {4, 30}, {5, 100}};
+//    vector<pair<int, int> > v5{{6, 10}, {7, 30}, {8, 20}, {9, 10}, {10, 100}};
+//    vector<pair<int, int> > v10{{11, 10}, {12, 10}, {13, 10}, {14, 10}, {15, 100}};
+//    vector<pair<int, int> > v15{{16, 10}, {17, 10}, {18, 10}, {19, 10}, {20, 10}};
+//    graph_type g(nodes_amount);
+//    g[0] = v0;
+//    g[5] = v5;
+//    g[10] = v10;
+//    g[15] = v15;
+//    runner.load_graph(g);
+//    runner.add_stop_task(16 * TestRunner::SLOT_MS);
+//    runner.run<GrandpaNode>();
+////    cout << get_block_height(runner.get_db(0).last_irreversible_block_id()) << endl;
+//    EXPECT_EQ(get_block_height(runner.get_db(0).last_irreversible_block_id()), 16);
+//    EXPECT_EQ(get_block_height(runner.get_db(5).last_irreversible_block_id()), 16);
+//    EXPECT_EQ(get_block_height(runner.get_db(10).last_irreversible_block_id()), 16);
+//    EXPECT_EQ(get_block_height(runner.get_db(15).last_irreversible_block_id()), 16);
+//}
