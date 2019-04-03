6b7f776b5080b043eee341fb1f9a16196849a28f
SCaptainCAP <SGV123@yandex.ru>
2019-04-03 11:29:08 +0300

    [bloc-92][cyb-99] telemetry plugin
    * [bloc-92] Generated plugin, added to CMakeLists
    
    * [bloc-92] Prometheus-cpp library added as submodule
    
    * [bloc-92] Added implementation of telemetry plugin
    
    * [bloc-92] Code style fixes
    
    * [bloc-92] Fixed prometheus-cpp library version

---
+++ plugins/telemetry_plugin/telemetry_plugin.cpp
@@ -0,0 +1,137 @@
+/**
+ *  @file
+ *  @copyright defined in cyberos/LICENSE
+ */
+#include <eosio/telemetry_plugin/telemetry_plugin.hpp>
+#include <fc/exception/exception.hpp>
+#include <eosio/chain/plugin_interface.hpp>
+#include <prometheus/exposer.h>
+
+#define LATENCY_HISTOGRAM_KEYPOINTS \
+    {1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 180000}
+
+
+namespace eosio {
+    using namespace chain::plugin_interface;
+    using namespace prometheus;
+
+    static appbase::abstract_plugin &_telemetry_plugin = app().register_plugin<telemetry_plugin>();
+
+    class telemetry_plugin_impl {
+    private:
+        channels::accepted_block::channel_type::handle _on_accepted_block_handle;
+        channels::irreversible_block::channel_type::handle _on_irreversible_block_handle;
+
+        std::unique_ptr<Exposer> exposer;
+        std::shared_ptr<Registry> registry;
+
+        std::unique_ptr<Counter> accepted_trx_count;
+        std::unique_ptr<Histogram> irreversible_latency_hist;
+        std::unique_ptr<Gauge> last_irreversible_latency;
+
+        void start_server() {
+            exposer = std::make_unique<Exposer>(endpoint, uri, threads);
+        }
+
+        void add_event_handlers() {
+            _on_accepted_block_handle = app().get_channel<channels::accepted_block>()
+                    .subscribe([this](block_state_ptr s) {
+                        accepted_trx_count->Increment(s.get()->trxs.size());
+                    });
+
+            _on_irreversible_block_handle = app().get_channel<channels::irreversible_block>()
+                    .subscribe([this](block_state_ptr s) {
+                        fc::microseconds latency = fc::time_point::now() - s.get()->header.timestamp.to_time_point();
+                        int64_t latency_millis = latency.count() / 1000;
+                        last_irreversible_latency->Set(latency_millis);
+                        irreversible_latency_hist->Observe(latency_millis);
+                    });
+        }
+
+        void add_metrics() {
+            registry = std::make_unique<Registry>();
+
+            accepted_trx_count.reset(
+                    &BuildCounter()
+                            .Name("accepted_trx_total")
+                            .Help("Total amount of transactions accepted")
+                            .Register(*registry)
+                            .Add({}));
+
+
+            irreversible_latency_hist.reset(
+                    &BuildHistogram()
+                            .Name("irreversible_latency")
+                            .Help("The latency of irreversible blocks")
+                            .Register(*registry)
+                            .Add({},
+                                 Histogram::BucketBoundaries{
+                                         LATENCY_HISTOGRAM_KEYPOINTS
+                                 })
+            );
+
+            last_irreversible_latency.reset(
+                    &BuildGauge()
+                            .Name("last_irreversible_latency")
+                            .Help("The latency of the last irreversible blocks")
+                            .Register(*registry)
+                            .Add({})
+            );
+
+
+            exposer->RegisterCollectable(std::weak_ptr<Registry>(registry));
+        }
+
+    public:
+        std::string endpoint;
+        std::string uri;
+        size_t threads{};
+
+        void initialize() {
+            start_server();
+            add_metrics();
+            add_event_handlers();
+        }
+
+        virtual ~telemetry_plugin_impl() = default;
+    };
+
+    telemetry_plugin::telemetry_plugin() : my(new telemetry_plugin_impl()) {}
+
+    telemetry_plugin::~telemetry_plugin() = default;
+
+    void telemetry_plugin::set_program_options(options_description &, options_description &cfg) {
+        cfg.add_options()
+                ("telemetry-endpoint", bpo::value<string>()->default_value("8080"),
+                 "the endpoint upon which to listen for incoming connections to promethus server")
+                ("telemetry-uri", bpo::value<string>()->default_value("/metrics"),
+                 "the base uri of the endpoint")
+                ("telemetry-threads", bpo::value<size_t>()->default_value(1),
+                 "the number of threads to use to process network messages to promethus server");
+    }
+
+    void telemetry_plugin::plugin_initialize(const variables_map &options) {
+        ilog("Initialize telemetry plugin");
+        try {
+            my->endpoint = options.at("telemetry-endpoint").as<string>();
+            my->uri = options.at("telemetry-uri").as<string>();
+            my->threads = options.at("telemetry-threads").as<size_t>();
+        }
+        FC_LOG_AND_RETHROW();
+    }
+
+    void telemetry_plugin::plugin_startup() {
+        wlog("Telemetry plugin startup");
+        try {
+            my->initialize();
+            ilog("Telemetry plugin started, started listening endpoint (port) ${endpoint} with uri ${uri}",
+                 ("endpoint", my->endpoint)("uri", my->uri));
+        }
+        FC_LOG_AND_RETHROW();
+    }
+
+    void telemetry_plugin::plugin_shutdown() {
+        wlog("Telemetry plugin shutdown");
+    }
+
+}
