d8d072b235e85709080f875d9c81140e55d5d0f0
SCaptainCAP <SGV123@yandex.ru>
2019-04-03 11:29:08 +0300

    [bloc-92][cyb-99] telemetry plugin
    * [bloc-92] Generated plugin, added to CMakeLists
    
    * [bloc-92] Prometheus-cpp library added as submodule
    
    * [bloc-92] Added implementation of telemetry plugin
    
    * [bloc-92] Code style fixes
    
    * [bloc-92] Fixed prometheus-cpp library version

---
+++ plugins/telemetry_plugin/include/eosio/telemetry_plugin/telemetry_plugin.hpp
@@ -0,0 +1,31 @@
+/**
+ *  @file
+ *  @copyright defined in cyberos/LICENSE
+ */
+#pragma once
+#include <appbase/application.hpp>
+
+namespace eosio {
+
+using namespace appbase;
+
+/**
+ *  This is a plugin, intended to create a prometheus server for getting telemetry (using prometheus PULL model)
+ */
+class telemetry_plugin : public appbase::plugin<telemetry_plugin> {
+public:
+   telemetry_plugin();
+   virtual ~telemetry_plugin();
+
+   APPBASE_PLUGIN_REQUIRES()
+   virtual void set_program_options(options_description&, options_description& cfg) override;
+
+   void plugin_initialize(const variables_map& options);
+   void plugin_startup();
+   void plugin_shutdown();
+
+private:
+   std::unique_ptr<class telemetry_plugin_impl> my;
+};
+
+}
