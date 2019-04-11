c46107a41c7a0d07c69005c1e0c3e58457ca1eb5
Emil Guseynov <theluckyemil@gmail.com>
2019-04-11 16:43:20 +0300

    [cyb-184] fix sync logic

--- simulator/include/grandpa.hpp
+++ simulator/include/grandpa.hpp
@@ -4,26 +4,66 @@
 #define SYNC_GRANDPA //SYNC mode
 #include <eosio/grandpa_plugin/grandpa.hpp>
 #include <mutex>
+#include <queue>
 
-
+using std::queue;
+using std::unique_ptr;
+using std::make_unique;
+using grandpa_ptr = std::unique_ptr<grandpa>;
 
 class GrandpaNode: public Node {
 public:
     explicit GrandpaNode(int id, Network && net, fork_db && db_, private_key_type private_key):
         Node(id, std::move(net), std::move(db_), std::move(private_key))
     {
-        init_channels();
-        init_providers();
-        init_grandpa();
-
+        init();
         prefix_tree_ptr tree(new prefix_tree(std::make_shared<tree_node>(tree_node {
             db.last_irreversible_block_id()
         })));
-        grandpa.start(tree);
+        grandpa_impl->start(tree);
+    }
+
+    void init() {
+        init_channels();
+        init_providers();
+        init_grandpa();
+    }
+
+    virtual void restart() override {
+        cout << "[Node] #" << id << " restarted " << endl;
+        init();
+        grandpa_impl->start(copy_fork_db());
+        auto runner = get_runner();
+        auto from = this->id;
+        for (uint32_t to = 0; to < runner->get_instances(); to++) {
+            int delay = runner->get_delay_matrix()[from][to];
+            if (from != to && delay != -1) {
+                on_new_peer_event(to);
+            }
+        }
+    }
+
+    prefix_tree_ptr copy_fork_db() {
+        tree_node_ptr root = std::make_shared<tree_node>(tree_node { db.last_irreversible_block_id() });;
+        prefix_tree_ptr tree(new prefix_tree(std::move(root)));
+        queue<fork_db_node_ptr> q;
+        q.push(db.get_root());
+        while (!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            for (const auto& adjacent_node : node->adjacent_nodes) {
+                auto new_block_id = adjacent_node->block_id;
+                tree->insert(chain_type{node->block_id, {new_block_id}},
+                        adjacent_node->creator_key,
+                        get_runner()->get_active_bp_keys());
+                q.push(adjacent_node);
+            }
+        }
+        return tree;
     }
 
     ~GrandpaNode() {
-        grandpa.stop();
+        grandpa_impl->stop();
     }
 
     void on_receive(uint32_t from, void* msg) override {
@@ -80,7 +120,8 @@ private:
     }
 
     void init_grandpa() {
-        grandpa
+        grandpa_impl = unique_ptr<grandpa>(new grandpa());
+        (*grandpa_impl)
             .set_event_channel(ev_ch)
             .set_in_net_channel(in_net_ch)
             .set_out_net_channel(out_net_ch)
@@ -100,6 +141,6 @@ private:
     lib_prodiver_ptr lib_prov;
     prods_provider_ptr prods_prov;
 
-    grandpa grandpa;
+    grandpa_ptr grandpa_impl;
 };
 
